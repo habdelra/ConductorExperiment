e5cc5dc89fd3498c9fc5a8efdfb14542616ea6cc
vineet-sinha <vineet.sinha@gmail.com>
2013-10-15 16:15:01 -0400

    got card rendering. loaded css and js

--- cards/card1.js
+++ cards/card1.js
@@ -1,7 +1,12 @@
+Conductor.require('/vendor/jquery/jquery.js');
+Conductor.requireCSS('/cards/card1.css');
 
 Conductor.card({
 	activate: function() {
-		//debugger;
+	},
+
+	render: function(intent, dimensions) {
+		$('body').text('hello world!');
 		window.alert("Hello!");
 	}
 });
