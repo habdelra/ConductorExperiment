45a087861f5d79e0723828814f88f8fd4352f439
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2013-10-16 13:48:20 -0400

    height service work

--- cards/card1.js
+++ cards/card1.js
@@ -3,6 +3,8 @@ Conductor.requireCSS('/cards/card1.css');
 
 Conductor.card({
 
+
+
 	metadata : {
 		test : function(){
 			return {
@@ -12,26 +14,46 @@ Conductor.card({
 	},
 
 	activate: function() {
+		// conductor does auto DOM observing for setting the dimensions. if you need to manipulate the DOM explicitely, then you need to disable this
+		//this.consumers.height.autoUpdate = false;
 	},
 
 	render: function(intent, dimensions) {
-		var $body = $('body');
+		var that = this,
+			$container = this.getContainerDiv();
+
 		//$body.text('hello world!');
-		if (this.data && this.data.person && this.data.person.name) $body.append($('<div/>').text('render: ' + this.data.person.name));
+		if (this.data && this.data.person && this.data.person.name) $container.append($('<div/>').text('render: ' + this.data.person.name));
 		this.consumers.xhr.request('get', '/data.json').then(function(responseBody){
 			var responseObj = JSON.parse(responseBody);
-			$body.append($('<div/>').text(responseObj.name));
+			$container.append($('<div/>').text(responseObj.name));
+			that.consumers.height.update({
+				width: 200,
+				height: 200
+			});
 		}, function(err){
 			//console.log(err);
 		});
 
+
+
 	},
 
 	didUpdateData : function(name, data){
-		var $body;
+		var $container = this.getContainerDiv();
 		if (name === '*' && data.person ){
-			$body = $('body');
-			$body.append($('<div/>').text('UpdateData: ' + data.person.name || 'none'));
+			$container.append($('<div/>').text('UpdateData: ' + data.person.name || 'none'));
 		}
+	},
+
+	getContainerDiv : function(){
+		var $body = $('body'),
+			$container = $body.find('.container');
+
+		if (!$container.length) {
+			$container = $('<div class="container"/>');
+			$body.append($container);
+		}
+		return $container;
 	}
 });
