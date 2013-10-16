bbe1c52c744cf1cfeae8e2cfde2cfd3a2c58a4d6
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2013-10-16 11:39:14 -0400

    working thru data loading in cards

--- app/views/pane.js
+++ app/views/pane.js
@@ -4,16 +4,35 @@ var PaneView = Ember.View.extend({
 
 	didInsertElement: function() {
 		console.log('card url: ' + this.get('controller.url'));
-		var card = new Conductor({
-			conductorURL: '/vendor/conductor.js.html'
-		}).load(this.get('controller.url'), this.get('controller.id'));
 
-	    card.appendTo(this.get('element')).
-	        then(function(card) {
-	        	card.render();
-	        	return card;
-	        }).
-	        fail(Ember.RSVP.rethrow);
+		var conductor = new Conductor({
+			conductorURL: '/vendor/conductor.js.html'
+		});
+
+		// conductor.loadData(this.get('controller.url'), this.get('controller.id'), {
+		// 	name: 'Hassan'
+		// });
+		var url = this.get('controller.url'),
+			id = this.get('controller.id');
+		$.getJSON('/data2.json').then(function(response){
+			setTimeout(function(){
+				conductor.loadData(url, id, response);
+			},1000);
+		});
+		conductor.loadData(url, id, {
+			"person" : {
+				"name": 'Luke'
+			}
+		});
+
+		var card = conductor.load(url, id);
+
+		card.appendTo(this.get('element')).
+            then(function(card) {
+              card.render();
+              return card;
+          }).
+        fail(Ember.RSVP.rethrow);
 
 	}
 
--- cards/card1.js
+++ cards/card1.js
@@ -8,14 +8,22 @@ Conductor.card({
 
 	render: function(intent, dimensions) {
 		var $body = $('body');
-		$body.text('hello world!');
+		//$body.text('hello world!');
+		if (this.data && this.data.person && this.data.person.name) $body.append($('<div/>').text('render: ' + this.data.person.name));
 		this.consumers.xhr.request('get', '/data.json').then(function(responseBody){
 			var responseObj = JSON.parse(responseBody);
 			$body.append($('<div/>').text(responseObj.name));
 		}, function(err){
-			debugger;
 			console.log(err);
 		});
 
+	},
+
+	didUpdateData : function(name, data){
+		var $body;
+		if (name === '*' && data.person ){
+			$body = $('body');
+			$body.append($('<div/>').text('UpdateData: ' + data.person.name || 'none'));
+		}
 	}
 });
