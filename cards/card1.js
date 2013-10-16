135c4a03ea3c02a71c0530e9917c148b122b8c98
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2013-10-16 10:56:41 -0400

    added explicit XHR get request

--- cards/card1.js
+++ cards/card1.js
@@ -2,11 +2,20 @@ Conductor.require('/vendor/jquery/jquery.js');
 Conductor.requireCSS('/cards/card1.css');
 
 Conductor.card({
+
 	activate: function() {
 	},
 
 	render: function(intent, dimensions) {
-		$('body').text('hello world!');
-		window.alert("Hello!");
+		var $body = $('body');
+		$body.text('hello world!');
+		this.consumers.xhr.request('get', '/data.json').then(function(responseBody){
+			var responseObj = JSON.parse(responseBody);
+			$body.append($('<div/>').text(responseObj.name));
+		}, function(err){
+			debugger;
+			console.log(err);
+		});
+
 	}
 });
